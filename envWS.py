c17f9d7b2c714432d0f8806f70fc4dbf213e57a9
ruszkipista <oi5672@gmail.com>
2021-09-10 00:56:53 +0100

    fix Procfile for Heroku deployment

--- envWS.py
+++ envWS.py
@@ -3,7 +3,7 @@
 import os
 # set temporary environment variables in the development system
 # Flask Web Server parameters
-os.environ.setdefault("FLASK_SECRET_KEY","juyfu fytflg ufy  jfufujuvcjuhvc")  # => Heroku Config Vars
+os.environ.setdefault("FLASK_SECRET_KEY","asefeoifmvpeojfg[poegj")  # => Heroku Config Vars
 os.environ.setdefault("FLASK_IP",         "127.0.0.1")
 os.environ.setdefault("PORT",             "5500")
 os.environ.setdefault("FLASK_DEBUG",      "True")
