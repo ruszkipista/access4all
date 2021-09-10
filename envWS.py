57e009f6458cc4937a48bfc2a4a0022d1b958b2f
ruszkipista <oi5672@gmail.com>
2021-09-10 01:08:38 +0100

    remove local config files from repo

--- envWS.py
+++
@@ -1,9 +0,0 @@
-# this file should not exist outside of the developer's machine
-#================================================================
-import os
-# set temporary environment variables in the development system
-# Flask Web Server parameters
-os.environ.setdefault("FLASK_SECRET_KEY","asefeoifmvpeojfg[poegj")  # => Heroku Config Vars
-os.environ.setdefault("FLASK_IP",         "127.0.0.1")
-os.environ.setdefault("PORT",             "5500")
-os.environ.setdefault("FLASK_DEBUG",      "True")
