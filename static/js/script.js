13114e6ff1cf2726a2f9ae0be9f524bc730897f6
slavabdev <slav.balykov@gmail.com>
2021-09-11 02:08:15 +0000

    added navbar

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+//Responsive hamburger menu
+$(document).ready(function () {
+    $('.header-burger').click(function (event) {
+        $('.header-burger, .header-menu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+});
